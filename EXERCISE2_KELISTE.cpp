a1b87787f83ddf0029f72a8650b95291a97c9234
Aicon Keliste <qahkeliste@tip.edu.ph>
2024-09-09 12:51:38 +0800

    EXERCISE2_KELISTE.cpp

--- EXERCISE2_KELISTE.cpp
+++ EXERCISE2_KELISTE.cpp
@@ -8,6 +8,12 @@ int main()
     cout << "                  ALGORITHM TO GET THE SUM, DIFFERENCE, PRODUCT, QUOTIENT AND REMAINDER OF TWO INTEGERS" << endl << endl;
     cout << "X = 4                 ";
     cout << " Y = 2" << endl << endl << endl;
+//PROCESS
+    //sum = x + y;
+    //difference = x - y;
+    //product = x * y;
+    //quotient =  x / y;
+    //remainder = x % y;
 //SUM
     cout << "SUM (+)"<< endl;
     cout << "X + Y = " << x + y << endl << endl;
