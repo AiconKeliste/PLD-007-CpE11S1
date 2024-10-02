b6ca564b21e146dc6dc55806f5e3e6b1f83bd261
Aicon Keliste <qahkeliste@tip.edu.ph>
2024-10-02 12:31:53 +0800

    Add files via upload

---
+++ PRACTICAL.cpp
@@ -0,0 +1,26 @@
+#include <iostream>	
+using namespace std;
+// 2. Create a C/C++ program that will produce accept five numbers and determine the sum of first and last number among the five numbers entered using while loop
+int main()
+{
+	int x, y, z, a, b = 0;
+	
+	cout << "Enter First Number: ";
+	cin >> x;
+	cout << "Enter Second Number: ";
+	cin >> y;
+	cout << "Enter Third Number: ";
+	cin >> z;
+	cout << "Enter Fourth Number: ";
+	cin >> a;
+	cout << "Enter Fifth Number: ";
+	cin >> b;
+	
+	while (x,b){
+	 int sum = 	x + b;
+		cout << "The sum of first and last is: "<< sum << endl;
+		x + b;
+		break;
+	}
+	return 0;
+}	 
